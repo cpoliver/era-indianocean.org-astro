2a065437d6553164b3e4602b8538f37f043efc0a
Charlie Oliver <111855352+cpoliver-perenna@users.noreply.github.com>
2024-04-27 01:26:40 +0400

    feat: update navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,183 +1,112 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
-
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'About us',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'What we do',
+          href: '/about/what-we-do',
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Meet our team',
+          href: '/about/meet-our-team',
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Our partners',
+          href: '/about/our-partners',
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Our donors',
+          href: '/about/our-donors',
         },
       ],
     },
     {
-      text: 'Pages',
+      text: 'News & insights',
+      href: '/news-and-insights',
+    },
+    {
+      text: 'Our work',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'Mauritius fruit bat conservation',
+          href: '/projects/mauritius-fruit-bat-conservation',
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Insectivorous bat conservation',
+          href: '/projects/insectivorous-bat-conservation',
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'Forest restoration',
+          href: '/projects/forest-restoration',
         },
         {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: 'Educational community outreach',
+          href: '/projects/educational-community-outreach',
         },
       ],
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
+      text: 'Biodiversity',
+      href: '/biodiversity',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'About us',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        {
+          text: 'What we do',
+          href: '/about/what-we-do',
+        },
+        {
+          text: 'Our team',
+          href: '/about/meet-our-team',
+        },
+        {
+          text: 'Partners',
+          href: '/about/partners',
+        },
+        {
+          text: 'Donors',
+          href: '/about/donors',
+        },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Our projects',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        {
+          text: 'Mauritius fruit bats',
+          href: '/projects/mauritius-fruit-bat-conservation',
+        },
+        {
+          text: 'Insectivorous bats',
+          href: '/projects/insectivorous-bat-conservation',
+        },
+        {
+          text: 'Forest restoration',
+          href: '/projects/forest-restoration',
+        },
+        {
+          text: 'Community outreach',
+          href: '/projects/educational-community-outreach',
+        },
       ],
     },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/ERAindianocean' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/eraindianocean?lang=en' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    ERA Indian Ocean &copy; 2016 &ndash; ${new Date().getFullYear()} · All rights reserved.
   `,
 };
